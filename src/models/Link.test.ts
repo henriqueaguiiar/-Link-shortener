5ff0b5ea44016da9a8cdc4636b6e453384267669
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-05-12 18:40:55 -0300

    TDD test link created

---
+++ src/models/Link.test.ts
@@ -0,0 +1,9 @@
+describe("Criação de links", () => {
+  test("Deve criar um link com sucesso", () => {
+    const link = new Link("cubosacademy", "https://cubosacademy");
+
+    expect(link).toHaveProperty("identificador", "cubosacademy");
+    expect(link).toHaveProperty("url", "https://cubosacademy");
+    expect(link).toHaveProperty("visitas", 0);
+  });
+});
